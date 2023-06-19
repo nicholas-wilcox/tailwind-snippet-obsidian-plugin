3d63d906f0cced5e2da0e25130d9df14a2ae5a99
Nicholas Wilcox <nwilcox@linicks.dev>
2023-06-19 08:45:02 -0500

    Replace sample plugin with code that runs TailwindCSS on the vault and outputs to a snippet

--- main.ts
+++ main.ts
@@ -1,86 +1,44 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, FileSystemAdapter, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import postcss from 'postcss';
+import autoprefixer from 'autoprefixer';
+import tailwindcss, { Config as TailwindConfig } from 'tailwindcss';
+import { version as tailwindVersion } from 'tailwindcss/package.json';
+import plugin from 'tailwindcss/plugin'
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface UnofficialTailwindPluginSettings {
+	enablePreflight: boolean;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: UnofficialTailwindPluginSettings = {
+	enablePreflight: false,
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class UnofficialTailwindPlugin extends Plugin {
+	settings: UnofficialTailwindPluginSettings;
+	preflightPlugin: NonNullable<TailwindConfig['plugins']>[number];
+
+	get tailwindConfig(): TailwindConfig {
+		const adapter = this.app.vault.adapter as FileSystemAdapter;
+		return {
+			content: this.app.vault.getMarkdownFiles().map(f => adapter.getFullPath(f.path)),
+			theme: {
+				extend: {},
+			},
+			plugins: this.settings.enablePreflight ? [this.preflightPlugin] : [],
+			corePlugins: {
+				preflight: false,
+			}
+		}
+	}
 
 	async onload() {
 		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
+		await this.initPreflightPlugin();
+		this.registerEvent(this.app.vault.on('modify', () => this.doTailwind()));
 		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
-	onunload() {
-
-	}
+	onunload() { }
 
 	async loadSettings() {
 		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
@@ -88,49 +46,63 @@ export default class MyPlugin extends Plugin {
 
 	async saveSettings() {
 		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
+		await this.doTailwind();
 	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+	async initPreflightPlugin() {
+		const preflight = this.app.vault.configDir.concat('/plugins/unofficial-obsidian-tailwindcss-plugin/preflight.css');
+		const preflightCss = await this.app.vault.adapter.read(preflight);
+		const preflightStyles = postcss.parse(preflightCss);
+
+		// This is an altered version of the original preflight plugin.
+		this.preflightPlugin = plugin(({ addBase }) => {
+			addBase([
+				// @ts-ignore
+				postcss.comment({
+					// @ts-ignore
+					text: `! tailwindcss v${tailwindVersion} | MIT License | https://tailwindcss.com`,
+				}),
+				// @ts-ignore
+				...preflightStyles.nodes,
+			])
+		});
 	}
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
+	async doTailwind() {
+		const vault = this.app.vault;
+		const adapter = vault.adapter as FileSystemAdapter;
+		const cssIn = vault.configDir.concat('/plugins/unofficial-obsidian-tailwindcss-plugin/tailwind.css');
+		const cssOut = vault.configDir.concat('/snippets/tailwind.css');
+
+		const result = await postcss([
+			tailwindcss(this.tailwindConfig),
+			autoprefixer
+		]).process(await adapter.read(cssIn), { from: cssIn, to: cssOut });
+		await adapter.write(cssOut, result.css);
 	}
 }
 
 class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+	plugin: UnofficialTailwindPlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: UnofficialTailwindPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
-
+		const { containerEl } = this;
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
+			.setName('Enable Preflight')
+			.setDesc(`Adds TailwindCSS's Preflight to the generated stylesheet.
+					 (NOTE: Not all styles from Preflight will be applied.
+					  Also, Obsidian's UI will be affected.)`)
+			.addToggle(toggle => toggle
+				.setValue(this.plugin.settings.enablePreflight)
+				.onChange(async (value: boolean) => {
+					this.plugin.settings.enablePreflight = value;
 					await this.plugin.saveSettings();
 				}));
 	}
